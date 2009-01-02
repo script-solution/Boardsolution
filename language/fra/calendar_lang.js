a1db4996ed5e3bc947a8ca165dd68f616072a8eb
Nils Asmussen <nils@script-solution.de>
2009-01-02 13:38:20 +0000

    Integrated the language france (nearly finished)

---
+++ language/fra/calendar_lang.js
@@ -0,0 +1,33 @@
+/**
+ * The calendar-language-file
+ *
+ * @version			$Id: calendar_lang.js 49 2008-07-30 12:35:41Z nasmussen $
+ * @package			Boardsolution
+ * @subpackage	language.ger_du
+ * @author			Nils Asmussen <nils@script-solution.de>
+ * @copyright		2003-2008 Nils Asmussen
+ * @link				http://www.script-solution.de
+ */
+
+FWS_Calendar.language['wd_0'] = 'Mo';
+FWS_Calendar.language['wd_1'] = 'Tu';
+FWS_Calendar.language['wd_2'] = 'We';
+FWS_Calendar.language['wd_3'] = 'Th';
+FWS_Calendar.language['wd_4'] = 'Fr';
+FWS_Calendar.language['wd_5'] = 'Sa';
+FWS_Calendar.language['wd_6'] = 'Su';
+
+FWS_Calendar.language['month_0'] = 'January';
+FWS_Calendar.language['month_1'] = 'February';
+FWS_Calendar.language['month_2'] = 'March';
+FWS_Calendar.language['month_3'] = 'April';
+FWS_Calendar.language['month_4'] = 'May';
+FWS_Calendar.language['month_5'] = 'June';
+FWS_Calendar.language['month_6'] = 'July';
+FWS_Calendar.language['month_7'] = 'August';
+FWS_Calendar.language['month_8'] = 'September';
+FWS_Calendar.language['month_9'] = 'October';
+FWS_Calendar.language['month_10'] = 'November';
+FWS_Calendar.language['month_11'] = 'December';
+
+FWS_Calendar.language['close'] = 'Close';
