e7ffbca33632b6fe3dc7571deff193909b5ed8e8
Nils Asmussen <nils@script-solution.de>
2009-01-10 13:46:01 +0000

    Fixed that intern forums have been listed at fav-forums

--- front/module/userprofile/sub_favforums.php
+++ front/module/userprofile/sub_favforums.php
@@ -44,6 +44,7 @@ final class BS_Front_SubModule_userprofile_favforums extends BS_Front_SubModule
 		$user = FWS_Props::get()->user();
 		$forums = FWS_Props::get()->forums();
 		$tpl = FWS_Props::get()->tpl();
+		$auth = FWS_Props::get()->auth();
 		// collect the forum-ids
 		$forum_ids = array();
 		foreach(BS_DAO::get_unreadhide()->get_all_of_user($user->get_user_id()) as $data)
@@ -69,24 +70,26 @@ final class BS_Front_SubModule_userprofile_favforums extends BS_Front_SubModule
 			$data = $forum->get_data();
 			$id = $forum->get_id();
 			/* @var $data BS_Forums_NodeData */
-
-			if(!isset($sub_cats[$data->get_parent_id()]))
-				$sub_cats[$data->get_parent_id()] = 1;
-			else
-				$sub_cats[$data->get_parent_id()]++;
-			
-			$tplforums[] = array(
-				'name' => $data->get_name(),
-				'id' => $id,
-				'index' => $index,
-				'contains_forums' => $data->get_forum_type() == 'contains_cats',
-				'path_images' => BS_ForumUtils::get_path_images($forum,$sub_cats,$images,1),
-				'forum_url' => BS_URL::build_topics_url($id),
-				'selected' => in_array($id,$forum_ids)
-			);
-			
-			if($data->get_forum_type() == 'contains_threads')
-				$index++;
+			if(!$data->get_forum_is_intern() || $auth->has_access_to_intern_forum($id))
+			{
+				if(!isset($sub_cats[$data->get_parent_id()]))
+					$sub_cats[$data->get_parent_id()] = 1;
+				else
+					$sub_cats[$data->get_parent_id()]++;
+				
+				$tplforums[] = array(
+					'name' => $data->get_name(),
+					'id' => $id,
+					'index' => $index,
+					'contains_forums' => $data->get_forum_type() == 'contains_cats',
+					'path_images' => BS_ForumUtils::get_path_images($forum,$sub_cats,$images,1),
+					'forum_url' => BS_URL::build_topics_url($id),
+					'selected' => in_array($id,$forum_ids)
+				);
+				
+				if($data->get_forum_type() == 'contains_threads')
+					$index++;
+			}
 		}
 		
 		$tpl->add_variable_ref('forums',$tplforums);
