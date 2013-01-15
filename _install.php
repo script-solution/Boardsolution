1df8b5b35c11d30c42e19f062028e3cc2437ca9f
Rafioso <rafioso@gtavision.com>
2013-01-15 22:46:21 +0100

    Again some default-folder and files

---
+++ _install.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The index-page for the install-script
+ * 
+ * @package			Boardsolution
+ * @subpackage	main
+ *
+ * Copyright (C) 2003 - 2012 Nils Asmussen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+define('BS_PATH','');
+
+include_once(BS_PATH.'config/userdef.php');
+
+// define fwspath for init.php
+if(!defined('FWS_PATH'))
+	define('FWS_PATH',BS_PATH.BS_FWS_PATH);
+
+// init the framework
+include_once(FWS_PATH.'init.php');
+
+// set the path
+FWS_Path::set_server_app(BS_PATH);
+FWS_Path::set_client_app(BS_PATH);
+// Note that we don't need the outer-path here
+
+// init boardsolution
+include_once(BS_PATH.'src/autoloader.php');
+FWS_AutoLoader::register_loader('BS_Autoloader');
+
+// include the files that we need at the very beginning
+include_once(BS_PATH.'config/general.php');
+include_once(BS_PATH.'src/props.php');
+
+// set the accessor and loader for boardsolution
+$accessor = new BS_Install_PropAccessor();
+$accessor->set_loader(new BS_Install_PropLoader());
+FWS_Props::set_accessor($accessor);
+
+// now render document
+$doc = FWS_Props::get()->doc();
+echo $doc->render();
+?>
