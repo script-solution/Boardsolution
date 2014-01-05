6698ed725580dc6b2c7ae12e14085bc923697e82
Rafioso <rafioso@gtavision.com>
2014-01-06 00:43:20 +0100

    Just formatting

--- install/module/3/module.php
+++ install/module/3/module.php
@@ -65,7 +65,8 @@ final class BS_Install_Module_3 extends BS_Install_Module
 			'PHP-Version',$status['php'],0,0,$values['php']
 		);
 		$configs[] = $this->get_status(
-			'MySQL-Version',($values['mysql']===true)?false:$status['mysql'],0,0,($values['mysql']===true)?$locale->lang('mysql_version_to_check'):$values['mysql']
+			'MySQL-Version',($values['mysql']===true) ? false : $status['mysql'],0,0,
+				            ($values['mysql']===true) ? $locale->lang('mysql_version_to_check') : $values['mysql']
 		);
 		$configs[] = $this->get_status(
 			'GD-Library',$status['gd'],0,0,$values['gd'],$locale->lang('gd_description'),'warning'
